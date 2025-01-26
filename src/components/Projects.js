27145d1a72e8f02c4337e54566d0a40dcadbc317
valeriya <valeriyastoyan26@gmail.com>
2025-01-26 20:12:46 +0300

    corr Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -51,7 +51,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>On this site, I've compiled a list of my front-end projects published on GitHub that can help you improve your proficiency in HTML, CSS, JavaScript, ReactJs, and other front-end technologies.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
