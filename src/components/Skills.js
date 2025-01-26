35dd482145ccb923da9abd67ce1dc0f7a94fe1f8
valeriya <valeriyastoyan26@gmail.com>
2025-01-26 19:55:53 +0300

    add skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -39,19 +39,19 @@ export const Skills = () => {
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>Styles CSS</h5>
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <h5>JavaScript</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>React JS</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>TypeScript</h5>
                             </div>
                         </Carousel>
                     </div>
