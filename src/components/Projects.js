a2603871e750822c88db66c2dc9d0e7e5ef59947
Valeriya266 <127612875+Valeriya266@users.noreply.github.com>
2025-01-27 10:40:56 +0300

    corr text of Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,8 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/1.jpg";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+import projImg2 from "../assets/img/2.jpg";
+import projImg3 from "../assets/img/3.jpg";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -36,7 +36,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>On this site, I've compiled a list of my front-end projects published on GitHub that can help you improve your proficiency in HTML, CSS, JavaScript, ReactJs and other front-end technologies.</p>
+                <p>On this site, I've compiled a list of my front-end projects published on GitHub that let you evaluate my proficiency in HTML/CSS, JavaScript, ReactJs and other front-end technologies.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                     <Tab.Pane eventKey="first">
