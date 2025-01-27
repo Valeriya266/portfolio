0ed579e1e8dc352b87a49b75e791b62f5d598c60
valeriya <valeriyastoyan26@gmail.com>
2025-01-27 20:45:35 +0300

    corr imgs

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,7 @@ export const ProjectCard = ({ title, description, imgUrl, link }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-       <a href="{link}"><img src={imgUrl}</a> />
+       <a href="{link}"><img src={imgUrl}></img></a>
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
